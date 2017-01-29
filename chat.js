d633e7fa5d2cef411683e27914a09b15b410d27a
deepak2136 <deepakprajapati589@gmail.com>
2017-01-29 21:11:27 +0530

    a

--- chat.js
+++ chat.js
@@ -5,12 +5,21 @@ ifUserIsLoggedIn(function(){
     var usersList = "";
     for(var uid in users){
       var user = users[uid];
+
+      /* if user id over id then not add in user list */
       if( window.currentUser.id != uid ){
         usersList += renderUser(user);
-      }      
+      }
     }
 
     /* adding run time elemets in web page */
     getElement("members").innerHTML = usersList;
   });
 });
+
+function getChatId(id1, id2) {
+  if(id1 > id2){
+    return id1 + "" + id2;
+  }
+  return id2 +"" + id1;
+}
